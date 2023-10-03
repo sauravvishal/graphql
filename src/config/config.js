1f775d438f88ad5b077dbaa6ceafb9eac7bb8042
Saurav Vishal <saurav.vishal@overdose.digital>
2023-10-03 17:20:43 +0530

    changed ct creds

--- src/config/config.js
+++ src/config/config.js
@@ -1,11 +1,11 @@
 module.exports = {
     PORT: 3001,
-    DEV_PROJECT_KEY: "sauravvishal-training",
-    DEV_CLIENT_SECRET: "9RRV18X7pmy5B4wAmwQU9yfVFkAAcpY7",
-    DEV_CLIENT_ID: "TDauP2yJUDlbMHwlCwu9waxa",
+    DEV_PROJECT_KEY: "graphql-commerce-tool",
+    DEV_CLIENT_SECRET: "C0lImgh_ejrFIuQO4S6A5iSawpN0SEyJ",
+    DEV_CLIENT_ID: "m5ngYp3SbxzPAy2P0I4YiA1p",
     DEV_AUTH_URL: "https://auth.us-central1.gcp.commercetools.com",
     DEV_API_URL: "https://api.us-central1.gcp.commercetools.com",
-    DEV_SCOPES: "manage_project:sauravvishal-training",
+    DEV_SCOPES: "manage_project:graphql-commerce-tool",
     DEV_CUSTOMER_EMAIL: "testvishal@example.com",
     DEV_CUSTOMER_PASSWORD: "abcd1234"
 }
